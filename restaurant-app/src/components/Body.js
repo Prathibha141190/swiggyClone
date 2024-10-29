a8ff12ad14705d4d25e26cb2c7aa6db8608dfab6
prathibha141190 <prathibhasony5@gmail.com>
2024-10-29 12:48:56 +0530

    updateting components

--- restaurant-app/src/components/Body.js
+++ restaurant-app/src/components/Body.js
@@ -4,15 +4,12 @@ import '../App.css'
 import Card, { HOC } from "./Card"
 import {Link} from 'react-router-dom'
 import useGetdata from '../utills/useGetData'
-import ScrolledItems from "./scrolledItems"
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-
-import { Swiper, SwiperSlide } from 'swiper/react';
-
+import ScrolledItems from './scrolledItems'
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
+
 function Body(){
 
 let [searchData,setSearchData]=useState('')
@@ -24,9 +21,9 @@ let url="https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.37240&lng=78.437
 let restData=useGetdata(url)
     let restauranArr=restData?.data?.cards[4].card?.card?.gridElements?.infoWithStyle.restaurants
 
-    let scrolledItemsArr=restData?.data?.cards[0].card?.card?.imageGridCards?.info
 
-    console.log(scrolledItemsArr)
+
+
     
 let imgUrl="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/"
 
@@ -55,40 +52,9 @@ setFilterData(ratedRestaur)
 }} className="bg-blue-500 rounded-md  p-1 m-1 ">topRatedRestaurants</button>
 </div>
 
- <div className='d-flex flex-col '>
-<div className="p-2 ">
-    <h1 className="font-bold font-sans text-2xl">What's on your mind?</h1>
+<ScrolledItems />
 
-      
-        <div className="items-center p-24 pb-1 pt-2">
-               <Swiper
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        spaceBetween={5}
-        slidesPerView={8}
-        navigation
-    
-      
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log('slide change')}
-    >
-        {scrolledItemsArr?.map((each)=>{
-              let {imageId,description}=each
-        return (
-            
-          <div className="h-36 w-52 ">
-<SwiperSlide><img src={imgUrl+imageId} className='h-36 w-44'/> </SwiperSlide>
 
-</div>
-        )
-        })
-
-}
-    </Swiper>
-        
-   
-        </div>
-      
-</div>
 <div className="d-flex flex-wrap p-1">
 {filterData?.map((eachresta)=>{
              console.log(eachresta)     
@@ -98,7 +64,7 @@ setFilterData(ratedRestaur)
         })}
     </div>
     </div>
-    </div>
+    
 )
 
 
--- restaurant-app/src/components/scrolledItems.js
+++ restaurant-app/src/components/scrolledItems.js
@@ -1,15 +1,57 @@
 import React from 'react';
-
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import useGetData from '../utills/useGetData';
 
-export default function ScrolledItems (){
+function ScrolledItems (){
 let url="https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.37240&lng=78.43780&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-   
+
+let imgUrl="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/"  
+
 let scrollData=useGetData(url)
 
+let scrolledItemsArr=scrollData?.data?.cards[0].card?.card?.imageGridCards?.info
 
+console.log(scrolledItemsArr)
 
-};
-  
+return(
+<div>
+<div className='d-flex flex-col '>
+<div className="p-2 ">
+    <h1 className="font-bold font-sans text-2xl">What's on your mind?</h1>
 
+      
+    <div className="items-center p-24 pb-1 pt-2">
+               <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={5}
+        slidesPerView={8}
+        navigation
+    
+      
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+    >
+        {scrolledItemsArr?.map((each)=>{
+              let {imageId,description}=each
+        return (
+            
+          <div className="h-36 w-52 ">
+<SwiperSlide><img src={imgUrl+imageId} className='h-36 w-44'/> </SwiperSlide>
 
+</div>
+        )
+        })
+
+}
+    </Swiper>
+        
+   
+        </div>
+      
+</div>
+</div>
+</div>
+)
+}
+export default ScrolledItems
